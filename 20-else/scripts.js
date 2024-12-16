118e12a615c6e1b68906cbd816f2f4d2b436d2c7
Yuri Rezende <yuri.rezende@souunit.com.br>
2024-12-16 18:37:54 -0300

    .

--- 20-else/scripts.js
+++
@@ -1,2 +0,0 @@
-console.log("Olá");
-console.log("Olá");
---
+++ 21_else_if/scripts.js
@@ -0,0 +1,10 @@
+let nome = "Matheus";
+let idade = 28;
+
+if (nome != undefined && nome == "Joaquim"){
+    console.log("Nome está definido");
+}else if(nome == "Matheus" && nome.length > 5 && idade == 50){
+    console.log("O nome é Matheus");
+}else {
+    console.log("Não é Matheus");
+}
