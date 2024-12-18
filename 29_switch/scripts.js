3bcb1cb9288676f81fe0fbf6662fe36da8bc8c93
Yuri Rezende <yuri.rezende@souunit.com.br>
2024-12-18 09:37:46 -0300

    .

---
+++ 29_switch/scripts.js
@@ -0,0 +1,13 @@
+let nome = "Matheus";
+
+switch(nome){
+    case "Matheus":
+        console.log("O nome é Matheus");
+        break;
+    case "João":
+        console.log("O nome é João");
+        break;
+    default:
+        console.log("O nome não foi encontrado");
+        break;
+}
